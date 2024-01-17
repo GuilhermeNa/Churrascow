f2de9292e1ed143cde5a9b2210669fcdeb13e611
GuilhermeNa <napoleao.gns@hotmail.com>
2024-01-17 19:55:36 -0300

    v0.1.3 - ValidationService optimization

---
+++ app/src/main/java/br/com/apps/churrascow/util/Constants.kt
@@ -0,0 +1,18 @@
+package br.com.apps.churrascow.util
+
+//---------------------------------------------------------------------------------------------//
+// ERRORS
+//---------------------------------------------------------------------------------------------//
+
+const val USER_ERROR = "Falha na criação"
+const val INTERNAL_ERROR = "Erro interno"
+const val EMPTY_FIELD = "Preencha o campo"
+const val STRING_TOO_BIG = "Atingiu o máximo de caracteres"
+const val DTO_NOT_FOUND = "Falha ao registrar, Dto não encontrado"
+const val SUMMARY_NOT_FOUND = "Falha ao registrar, Action Summary nao encontrado"
+
+//---------------------------------------------------------------------------------------------//
+// SUCCESS
+//---------------------------------------------------------------------------------------------//
+
+const val SUCCESSFULLY_CREATED = "Item criado com sucesso"
